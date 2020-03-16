ace016ffaae5a306f25a281bbd93024cb44bb71e
kzz <kzz@kzzdeMacBook-Pro.local>
2020-03-16 22:44:13 +0800

    两数相加

--- 2_两数相加.py
+++ 2_两数相加.py
@@ -11,3 +11,38 @@
 输出：7 -> 0 -> 8
 原因：342 + 465 = 807
 """
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        head = cur = ListNode(0)
+        first = True
+        carry = 0
+        while (l1 or l2 or carry != 0):
+            val1 = val2 = 0
+            if l1:
+                val1 = l1.val
+                l1 = l1.next
+            if l2:
+                val2 = l2.val
+                l2 = l2.next
+            sums = val1 + val2 + carry
+            if sums >= 10:
+                carry = 1
+                sums = sums % 10
+            else:
+                carry = 0
+            node = ListNode(sums)
+            if first:
+                head = cur = node
+                first = False
+            else:
+                cur.next = node
+                cur = cur.next
+        return head
